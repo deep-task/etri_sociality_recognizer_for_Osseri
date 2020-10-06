bbb6455f0b8a3c12e0b07ee9737aadd408215c16
JaeyoonJang <jangjy@etri.re.kr>
2020-10-06 09:38:39 +0900

    201006. change model download url

--- models/etri_model_download.sh
+++ models/etri_model_download.sh
@@ -12,11 +12,11 @@ then
 else
     cd $ETRI_RECOG_PATH'/models'
     rm *
-    FILEID='1DFsZrhRUeJ28NBjxhssjy0YggHSJZiNy'
-    FILENAME='ETRI_Osseri_Model.tar.xz'
+    FILEID='1m9fT413h1_glhX9-wO2Ia47h2QJnfzAb'
+    FILENAME='ETRI_Osseri_Model.tar.gz'
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id="$FILEID > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id="$FILEID -o $FILENAME
     rm cookie
     tar -zxvf $FILENAME
     echo "Modelfile Download Complete!!"
-fi
+fi 
