382329beccbfd56c9435463d4fc0a5ee27f337b9
jaeyoon <jangjy@etri.re.kr>
2020-12-21 10:46:24 +0900

    201221. add headpose&interest

--- models/etri_model_download.sh
+++ models/etri_model_download.sh
@@ -12,7 +12,7 @@ then
 else
     cd $ETRI_RECOG_PATH'/models'
     rm *
-    FILEID='1m9fT413h1_glhX9-wO2Ia47h2QJnfzAb'
+    FILEID='12uN2Hh8XkWAgGEzplPY4F6o6EKdtHlZ-'
     FILENAME='ETRI_Osseri_Model.tar.gz'
     curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id="$FILEID > /dev/null
     curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id="$FILEID -o $FILENAME
